ead958117aaf2cf3ff3e9707923b61eed6ab6a3b
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2022-07-01 17:27:57 -0400

    create setup of project

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import 'express-async-errors'
+import { routes } from './routes';
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(process.env.PORT || 3333, () => {
+    console.log('Server is running')
+})
